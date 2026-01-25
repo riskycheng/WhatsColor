64ac67c4fa2271bffbad3828cbc6c5c9d9c37b6b
Jian <riskycheng@gmail.com>
2026-01-25 12:36:22 +0800

    add initial version of swift

---
+++ WhatsColor/WhatsColor/WhatsColorApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhatsColorApp.swift
+//  WhatsColor
+//
+//  Created by Jian Cheng on 2026/1/25.
+//
+
+import SwiftUI
+
+@main
+struct WhatsColorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
